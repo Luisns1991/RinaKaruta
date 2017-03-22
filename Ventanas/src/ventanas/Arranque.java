9d30eeb0b570382c7d5c2cb0f5dc166fb46233da
Luis Navarro <luisns1991>
2017-03-22 17:45:27 -0500

    Carga Inicial

---
+++ Ventanas/src/ventanas/Arranque.java
@@ -0,0 +1,16 @@
+
+package ventanas;
+
+
+public class Arranque {
+
+  
+    public static void main(String[] args) {
+          
+        Index v1=new Index();
+        v1.setVisible(true);
+        
+    }
+    
+    
+}
